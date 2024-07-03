5c9ceca6ec7ab763ec0b8117d51d2aa20c7d6bee
MaceSAIT <166629082+MaceSAIT@users.noreply.github.com>
2024-07-02 19:11:41 -0600

    Initial Commit of stripped version to edit on PC

---
+++ Assignment 2/Assignment 2/TravelApp/Data/Flight.cs
@@ -0,0 +1,42 @@
+﻿using Blazorise;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelApp.Data
+{
+    public class Flight
+    {
+        public string FlightCode { get; set; }
+        public string Airline { get; set; }
+        public string Origin { get; set; }
+        public string Destination { get; set; }
+        public DayOfWeek Date { get; set; }
+        public string Time { get; set; }
+        public double Cost { get; set; }
+        public int SeatsAvailable { get; set; }
+
+        public Flight()
+        {
+
+        }
+        public Flight(string flightCode, string airline, string origin, string destination, string date, string time, double cost, int seatsAvailable)
+        {
+            FlightCode = flightCode;
+            Airline = airline;
+            Origin = origin;
+            Destination = destination;
+            Date = (DayOfWeek)Enum.Parse(typeof(DayOfWeek), date);
+            Time = time;
+            Cost = cost;
+            SeatsAvailable = seatsAvailable;
+        }
+
+        public override string ToString()
+        {
+            return $"{FlightCode} - {Airline} from {Origin} to {Destination} on {Date} at {Time}, Cost: {Cost:C}, Seats Available: {SeatsAvailable}";
+        }
+    }
+}
---
+++ Assignment 2/Assignment 2/TravelApp/Data/flightManager.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace TravelApp.Data
+{
+    public class flightManager
+    {
+
+        public List<Flight> flights = new List<Flight>();
+
+
+        public List<Flight> FindFlights(string originCode, string destinationCode, DayOfWeek date)
+        {
+            updateFlights();
+            var foundFlights = flights.Where(flight => flight.Origin == originCode && flight.Destination == destinationCode &&  flight.Date == date).ToList();
+            return foundFlights;
+        }
+
+        private void updateFlights()
+        {
+            string runningPath = Directory.GetCurrentDirectory();
+
+            // Construct the path to the flights.csv file
+            var filePath = Path.Combine(runningPath, "Resources", "res", "flights.csv");
+
+            // Ensure the file path is absolute
+            filePath = Path.GetFullPath(filePath);
+
+            string fileName = string.Format(filePath);
+
+            string[] fileLines = File.ReadAllLines(fileName);
+
+            
+
+            foreach (var line in fileLines)
+            {
+                if (line != "")
+                {
+                    string[] fileValues = line.Split(",");
+
+                    string flightCode = fileValues[0];
+                    string airline = fileValues[1];
+                    string originCode = fileValues[2];
+                    string destinationCode = fileValues[3];
+                    string date = fileValues[4];
+                    string time = fileValues[5];
+                    int seatsAvailable = int.Parse(fileValues[6]);
+                    double cost = double.Parse(fileValues[7]);
+
+                    flights.Add(new Flight(flightCode,airline,originCode,destinationCode,date,time,cost,seatsAvailable));
+                }
+            }
+        }
+
+        
+    }
+}
